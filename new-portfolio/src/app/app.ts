1417f7add27f972fd9ea3f7df4870da05610f478
Levi Josué Candeias de Figueiredo <levijcfigueiredowebdev@gmail.com>
2026-04-27 19:30:10 +0100

    feat: add new components for header, works, connections, contact, and download CV sections
    - Implemented Header component with navigation links.
    - Created Works component to showcase projects.
    - Added Connections component for social links.
    - Developed Contact component for user outreach.
    - Introduced Download CV component for resume download.
    - Updated main App component to include new sections.
    - Added respective CSS styles for each component.

--- new-portfolio/src/app/app.ts
+++ new-portfolio/src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
+import { Header } from './header/header';
+import { AboutMe } from './about-me/about-me';
+import { Works } from './works/works';
+import { DownloadCv } from './download-cv/download-cv';
+import { Connections } from './connections/connections';
+import { Contact } from './contact/contact';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, AboutMe, Works, DownloadCv, Connections, Contact],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
-  protected readonly title = signal('new-portfolio');
+  protected readonly title = signal('Portfolio');
 }
